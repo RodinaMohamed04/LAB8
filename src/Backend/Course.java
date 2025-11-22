3269ff06e88affeba06b9a0a50ac7b3aa64ed5b3
Ghadeer Mohamed <es.ghadir.mohamed2023@alexu.edu.eg>
2025-11-22 17:52:36 +0200

    edit for admin

--- src/Backend/Course.java
+++ src/Backend/Course.java
@@ -7,6 +7,7 @@ public class Course {
     protected String courseName;
     protected String courseDescription;
     protected int instructorId;
+    protected String status;
     protected ArrayList<Student> students;
     protected ArrayList<Lesson> lessons;
 
@@ -17,39 +18,62 @@ public class Course {
         this.instructorId = instructorId;
         this.students = new ArrayList<>();
         this.lessons = new ArrayList<>();
+        this.status = "Pending";
     }
+
     public String getCourseId() {
         return courseId;
     }
+
     public void setCourseId(String courseId) {
         this.courseId = courseId;
     }
+
     public String getCourseName() {
         return courseName;
     }
+
     public void setCourseName(String courseName) {
         this.courseName = courseName;
     }
+
     public String getCourseDescription() {
         return courseDescription;
     }
+
     public void setCourseDescription(String courseDescription) {
         this.courseDescription = courseDescription;
     }
+
     public int getInstructorId() {
-        return instructorId;}
+        return instructorId;
+    }
+
     public void setInstructorId(int instructorId) {
-        this.instructorId = instructorId;}
-    public  ArrayList<Student> getStudents() {
+        this.instructorId = instructorId;
+    }
+
+    public ArrayList<Student> getStudents() {
         return students;
     }
+
     public void setStudents(ArrayList<Student> students) {
         this.students = students;
     }
+
     public ArrayList<Lesson> getLessons() {
         return lessons;
     }
+
     public void setLessons(ArrayList<Lesson> lessons) {
         this.lessons = lessons;
     }
+}
+
+public String getStatus() {
+    return status;
+}
+
+public void setStatus(String status) {
+    this.status = status;
 }
