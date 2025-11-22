996ee565d6010cc0c4c34dae9f9624e5176a1d10
Ghadeer Mohamed <es.ghadir.mohamed2023@alexu.edu.eg>
2025-11-22 17:31:50 +0200

    admin 1

--- src/Backend/Admin.java
+++ src/Backend/Admin.java
@@ -1,5 +1,13 @@
 package Backend;
 
-public class Admin {
+public class Admin extends User {
+    public Admin(String userName, int userId, String email, String passwordHash) {
+        super(userName, userId, email, passwordHash, "admin");
 
+    }
+
+    public Admin(String userName, String email, String passwordHash) {
+        super(userName, email, passwordHash, "admin");
+
+    }
 }
